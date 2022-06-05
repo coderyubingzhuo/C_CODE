ba8bbaa635c2637f4059b97af80d3255ecc706f5
coderyubingzhuo <105156610+coderyubingzhuo@users.noreply.github.com>
2022-06-05 16:39:58 +0800

    Add files via upload
    比较较大值

---
+++ test_6_4/test_6_4/test_6_4.cpp
@@ -0,0 +1,36 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//int  max(int x, int y) {
+//
+//    if (x > y) 
+//
+//        return x;
+//
+//    
+//    else
+//
+//        return y;
+//
+//}
+//
+//
+//#include<stdio.h>
+//
+//
+//int main() {
+//
+//    int a = 10, b = 20, z;
+//
+//    z = max(a, b);
+//
+//    printf("%d\n", z);
+//
+//    return 0;
+//
+//}
+
+       
+
+    
+
+    
+
